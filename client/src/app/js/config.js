dea99a174d58b90ea0a24e276db8ea241c633bbe
ahr275 <abdallhhani123@gmail.com>
2026-02-07 02:39:27 +0300

    changing config file

--- client/src/app/js/config.js
+++ client/src/app/js/config.js
@@ -1,24 +1,2 @@
-// import POOL  from "pg";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-// import { fileURLToPath } from "url";
-
-// const Pool = POOL.Pool; 
-
-// needed if you're using ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({
-  path: path.resolve(__dirname, "../../process.env"),
-});
-
-
-const SERVER_URL = JSON.parse(process.env.VITE_API_URL);
-// console.log(dbConfig);
-// const pool = new Pool(dbConfig);
-
-
-
-export  {SERVER_URL}; 
+// src/js/config.js
+export const SERVER_URL = import.meta.env.VITE_API_URL;
