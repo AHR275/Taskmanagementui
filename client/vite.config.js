875907a2656fe041f770484233d3ff2ba4fd0323
ahr275 <abdallhhani123@gmail.com>
2026-02-07 02:31:43 +0300

    changing vite.config base

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-   base: "/Taskmanagementui/",
+   base: process.env.VITE_BASE_PATH || "/",
   resolve: {
     alias: {
       // Alias @ to the src directory
