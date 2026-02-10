a0577a71108821e5b1bd0286569aeadafa36bd2a
ahr275 <abdallhhani123@gmail.com>
2026-02-10 18:22:55 +0300

    fixing main.cron.js issues

--- backend/jobs/mian.cron.js
+++ backend/jobs/mian.cron.js
@@ -1,6 +1,11 @@
 import dailyStreak  from "./daily-streak.cron";
 
 
-async function main(params) {
+async function main() {
     dailyStreak();
 }
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
